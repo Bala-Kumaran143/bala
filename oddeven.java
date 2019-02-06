e2898c5d4428045853bce5e2be92d53e02e554a3
Bala-Kumaran143 <47377164+Bala-Kumaran143@users.noreply.github.com>
2019-02-06 15:36:27 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,16 @@
+class oddeven {
+
+    public static void main(String[] args) {
+
+        int number =22212;
+        if (number<0)
+            System.out.println("Invaild");
+
+        
+        else if ( number %2 == 0)
+            System.out.println("Even");
+
+        else
+            System.out.println("Odd");
+    }
+}
